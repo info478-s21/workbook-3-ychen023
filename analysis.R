332761734da3b2da3e2935aec1be976b310dbe5d
Yilin Chen <yicchen805@gmail.com>
2021-05-03 20:41:19 -0700

    finish wb3

--- analysis.R
+++ analysis.R
@@ -6,17 +6,39 @@ library(dplyr)
 library(ggplot2)
 
 # Create the `charts/` directory (you can do this from R!)
-
+dir.create('charts', showWarnings = FALSE)
 # Load prepped data
+health_data <- read.csv('./data/prepped/all_data.csv')
+# View(health_data)
 
 # Are HALE and life expectancy correlated?
 # - Plot 2016 life expectancy against 2016 HALE. Save the graph to `charts/`
 # - Compute the correlation between 2016 life expectancy against 2016 HALE
+data_2016 <- health_data %>%
+  filter(year == 2016)
+# View(data_2016)
 
+  ggplot(data_2016) +
+    geom_point(mapping = aes(x = le, y = hale)) +
+    labs(title = "Life Expectancy vs Hale",
+         x = "Life Expectancy", 
+         y = "HALE") 
+  
+  ggsave("charts/le_hale_plot.png")
+  
+cor(data_2016$hale, data_2016$le)
 # Are HALE and DALYs correlated?
 # - Plot 2016 HALE against 2016 DALYs. Save the graph to `charts/`
 # - Compute the correlation between 2016 HALE and DALYs
+ggplot(data_2016) +
+  geom_point(mapping = aes(x = dalys, y = hale)) +
+  labs(title = "DALYS vs Hale",
+       x = "DALYS", 
+       y = "HALE") 
 
+ggsave("charts/dalys_hale_plot.png")
+
+cor(data_2016$dalys, data_2016$hale)
 
 # As people live longer, do they live healthier lives 
 # (i.e., is a smaller fraction of life spent in poor health)?
@@ -26,11 +48,30 @@ library(ggplot2)
 # This will create `hale_2016`, `hale_1990`, `le_2016`, etc.
 # To do this, I suggest that you use the `pivot` function in the new
 # tidyverse release:https://tidyr.tidyverse.org/articles/pivot.html#wider
-
-
+data_wide <- health_data %>%
+  pivot_wider(names_from = year,
+              values_from = c(hale, le, dalys))
+# View(data_wide)
 # Create columns to store the change in life expectancy, and change in hale
-
+data_wide <- data_wide %>%
+  mutate(hale_diff = hale_2016 - hale_1990,
+         le_diff = le_2016 - le_1990)
 
 # Plot the *change in hale* against the *change in life expectancy*
 # Add a 45 degree line (i.e., where x = y), and save the graph to `charts/`
 # What does this mean?!?! Put your interpretation below
+ggplot(data_wide) +
+  geom_point(mapping = aes(x = le_diff, y = hale_diff)) +
+  labs(title = "Life Expectancy difference vs HALE difference",
+       x = "Change in Life Expectancy", 
+       y = "Change in HALE") +
+  geom_abline(intercept = 0, slope = 1) +
+  xlim(-15, 20) +
+  ylim(-15, 20)
+
+ggsave("charts/change_plot.png")
+
+### From the plot, it shows that as people are having larger life expectancy, 
+### they have a lower hale.
+
+
